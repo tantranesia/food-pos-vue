b6649c8de40cf8cc8f7015531297a47b5ca779d6
irmaaprilia <irmaapriliawulandari@gmail.com>
2021-10-11 09:38:48 +0700

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,66 +1,79 @@
 <template>
   <div>
-      <v-row>
-        <v-col :style="{background: $vuetify.theme.themes.light.black}" sm="7" class="pa-10">
-          <v-row>
-            <p class="display-1 white--text text--darken-2">Nasi Campur Mang ADE</p>
-            <v-text-field placeholder="Search"></v-text-field>
+    <v-row>
+      <v-col :style="{ background: $vuetify.theme.themes.light.black }" sm="7" class="pa-10">
+        <v-row>
+          <p class="display-1 white--text text--darken-2">Nasi Campur Mang ADE</p>
+          <v-text-field placeholder="Search"></v-text-field>
+        </v-row>
+        <p class="white--text text--darken-2">di rumah aja</p>
+        <p class="white--text text--darken-2">Wed Oct 06 2021</p>
+        <v-row mx="4">
+          <v-col sm="4" v-for="item in items" :key="item.menu_id">
+            <v-card class="card-home px-5 pb-5" color="base">
+              <v-img
+                :src="item.image"
+                class="mx-auto rounded-circle"
+                width="55%"
+                height="100px"
+                :z-index="zIndex"
+              />
+              <p class="white--text text--darken-2 text-center">{{ item.name }}</p>
+              <p class="white--text text--darken-2 text-center">Rp{{ item.price }}</p>
+              <p class="white--text text--darken-2 text-center">{{ item.description }}</p>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-app :style="{ background: $vuetify.theme.themes.light.base }" sm="5">
+        <v-col class="pt-10 mx-2">
+          <p class="display-1 white--text">Orders #34562</p>
+          <v-row align="center" justify="space-around" class="button-group">
+            <v-btn color="primary">Dine-In</v-btn>
+            <v-space></v-space>
+            <v-btn color="primary">Take Away</v-btn>
+            <v-space></v-space>
+            <v-btn color="primary">Delivery</v-btn>
+            <v-space></v-space>
+            <v-btn color="primary">Reservation</v-btn>
           </v-row>
-          <p class="white--text text--darken-2">di rumah aja</p>
-          <p class="white--text text--darken-2">Wed Oct 06 2021</p>
-          <v-row mx="4">
-            <v-col sm="4" v-for="item in items" :key="item.menu_id">
-              <v-card class="card-home px-5 pb-5" color="base">
-                <v-img
-                  :src="item.image"
-                  class="mx-auto rounded-circle"
-                  width="55%"
-                  height="100px"
-                  :z-index="zIndex"
-                />
-                <p class="white--text text--darken-2 text-center">{{ item.name }}</p>
-                <p class="white--text text--darken-2 text-center">Rp{{ item.price }}</p>
-                <p class="white--text text--darken-2 text-center">{{ item.description }}</p>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-app :style="{background: $vuetify.theme.themes.light.base}" sm="5">
-          <v-col class="pt-10 mx-2">
-            <p class="display-1 white--text">Orders #34562</p>
-            <v-row align="center" justify="space-around" class="button-group">
-               <v-btn color="primary">Dine-In</v-btn>
-               <v-space></v-space>
-              <v-btn color="primary">Take Away</v-btn>
-               <v-space></v-space>
-              <v-btn color="primary">Delivery</v-btn>
-               <v-space></v-space>
-               <v-btn color="primary">Reservation</v-btn>
-            </v-row>
-            <v-row class="my-6">
+          <v-row class="my-6">
             <p class="white--text">Item</p>
             <v-spacer></v-spacer>
             <p class="white--text mx-10">Qty</p>
             <p class="white--text">Price</p>
+          </v-row>
+          <v-flex>
+            <v-row v-for="item in items" :key="item.menu_id" sm="2">
+              <v-col>
+              <p class="white--text">{{ item.name }}</p>
+              <p class="white--text">Rp{{ item.price }}</p>
+              </v-col>
+              <div color="base" class="card-cart">
+              <p>1</p>
+              </div>
+              <p>Rp20000</p>
             </v-row>
-            <v-flex>
-            <v-row>
-            <v-image src="@/assets/image/nasi-goreng-jawa.jpeg"/>
-            <p class="white--text">Nasi Goreng Jawa</p>
-            </v-row>
-            </v-flex>
-          </v-col>
-        </v-app>
-      </v-row>
+            <v-btn color="primary" class="my-10 button-order">
+            Confirm Order
+            </v-btn>
+          </v-flex>
+        </v-col>
+      </v-app>
+    </v-row>
   </div>
 </template>
 <script>
 import axios from 'axios'
+import image from '@/assets/image/nasi-goreng-jawa.jpeg'
+import trash from '@/assets/image/Button.svg'
 
 export default {
   data() {
     return {
       items: [],
+      image,
+      trash,
     }
   },
 
@@ -86,6 +99,21 @@ export default {
   position: relative;
 }
 .button-group {
-flex-wrap: wrap
+  flex-wrap: wrap;
+}
+.image-cart {
+width: 3%;
+min-width: 2px;
+}
+.button-order {
+min-width: full
+}
+.card-cart {
+border: 1px solid #393C49;
+width: 50px;
+height: 50px;
+border-radius: 10px;
+mx: 5px
+
 }
 </style>
